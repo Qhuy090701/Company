b37fefccd059f3d6f7635cc859219eedd7ba4518
Quang Huy <tqhuy9701@gmail.com>
2023-06-02 01:39:44 +0700

    statemachine bot
    statemachine bot

--- Assets/Game/Gameplay/Scripts/StateMachineBot.cs
+++ Assets/Game/Gameplay/Scripts/StateMachineBot.cs
@@ -9,6 +9,7 @@ public class StateMachineBot : MonoBehaviour
     [SerializeField] private BotState botstate;
     [SerializeField] private GameObject currenttarget;
     private RunningGame runningGame;
+    private FightGame fightGame;
     private CanvasSetting canvasSetting;
     private PlayerFight playerFight;
     private enum BotState
@@ -19,6 +20,7 @@ public class StateMachineBot : MonoBehaviour
     }
     private void Awake()
     {
+        fightGame = FindAnyObjectByType<FightGame>();
         playerFight = FindAnyObjectByType<PlayerFight>();
         runningGame = FindAnyObjectByType<RunningGame>();
         canvasSetting  = FindAnyObjectByType<CanvasSetting>();
@@ -44,11 +46,33 @@ public class StateMachineBot : MonoBehaviour
 
     private void Move()
     {
-        if(currenttarget == null)
+        if (currenttarget == null)
         {
-          
+            // Tìm vị trí có player
+            foreach (Transform position in fightGame.listPosition)
+            {
+                PosMatrix posMatrix = position.GetComponent<PosMatrix>();
+                if (posMatrix != null && posMatrix.isHavePlayer)
+                {
+                    currenttarget = position.gameObject;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            // Di chuyển đến vị trí có player
+            transform.position = Vector3.MoveTowards(transform.position, currenttarget.transform.position, speed * Time.deltaTime);
+
+            // Nếu đã đến vị trí có player, chuyển state về wait
+            if (transform.position == currenttarget.transform.position)
+            {
+                currenttarget = null;
+                botstate = BotState.wait;
+            }
         }
     }
+
 
     private void OnTriggerEnter(Collider other)
     {
